b104ec8cc54b685d428060fc4dca133ae9785a2b
(Song) <junun24@gmail.com>
2022-06-02 12:12:56 +0900

    reat-test

---
+++ src/hello.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from 'styled-components';
+
+
+function Hello(){
+    constStyleButton = styled.button`
+        color:red;
+        backgrond-color:gray;
+
+    `
+    return <StyledButton>나만의 버튼</StyledButton>
+}
+export default Hello;
